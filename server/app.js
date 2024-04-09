bc2315d05bf4414396d15881261f1341743b700e
vvssgowtham <gowthamvvss@gmail.com>
2024-04-09 17:56:43 +0530

    Implemented server

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const port = 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://visualize-beta.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
